e4eab25bd584cbb294e597d43ff3793cead85ba6
edcaron <eduardocaron10@gmail.com>
2016-04-05 03:30:11 -0300

    auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,18 +26,17 @@ Route::get('/', function () {
 |
 */
 
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth(); 
     Route::get('/home', 'HomeController@index');
-});
+});	
 
 Route::group([ 'middleware' => 'web', 'prefix'=>'produtos', 'where'=>['id'=>'[0-9]+']], function() {
-	Route::get('',['as'=>'produtos', 'uses'=>'ProdutosController@index']);
-	Route::get('create',['as'=>'produtos.create', 'uses'=>'ProdutosController@create']);
-	Route::post('store',['as'=>'produtos.store', 'uses'=>'ProdutosController@store']);
-	Route::get('{id}/destroy',['as'=>'produtos.destroy', 'uses'=>'ProdutosController@destroy']);
-	Route::get('{id}/edit',['as'=>'produtos.edit', 'uses'=>'ProdutosController@edit']);
-	Route::put('{id}/update',['as'=>'produtos.update', 'uses'=>'ProdutosController@update']);
+	Route::get('',['middleware' => 'auth', 'as'=>'produtos', 'uses'=>'ProdutosController@index']);
+	Route::get('create',['middleware' => 'auth', 'as'=>'produtos.create', 'uses'=>'ProdutosController@create']);
+	Route::post('store',['middleware' => 'auth', 'as'=>'produtos.store', 'uses'=>'ProdutosController@store']);
+	Route::get('{id}/destroy',['middleware' => 'auth', 'as'=>'produtos.destroy', 'uses'=>'ProdutosController@destroy']);
+	Route::get('{id}/edit',['middleware' => 'auth', 'as'=>'produtos.edit', 'uses'=>'ProdutosController@edit']);
+	Route::put('{id}/update',['middleware' => 'auth', 'as'=>'produtos.update', 'uses'=>'ProdutosController@update']);
 });
 
--- app/Produto.php
+++ app/Produto.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     protected $fillable = ['nome','descricao'];	
+
+    public function avaliacoes()
+	{
+		return $this->hasMany('App\AvaliacaoProduto');
+	}
+
+	public function tags()
+	{
+		return $this->belongsToMany('App\Tag');
+	}
 }
