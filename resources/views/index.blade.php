0b43bb9bf181d1af8d5efc2a39c362aded46ae88
Klaus Etgeton <klausetgeton@gmail.com>
2016-04-11 21:54:07 -0300

    Adicionando os links para autenticar e deslogar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,12 +50,16 @@
                     <li><a href="{{ url('/home') }}">Home</a></li>
                 </ul>
 
+                 <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                </ul>
+
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                        <li><a href="{{ url('/auth/register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -63,7 +67,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{ url('/auth/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
                     @endif
